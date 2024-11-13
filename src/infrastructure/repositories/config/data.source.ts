0afc5993477f8fbd0e4c75b38104379850d7f029
Marcela <ardilamarcela42@gmail.com>
2024-11-13 12:25:07 -0500

    Agregando variables de entorno

---
+++ src/infrastructure/repositories/config/data.source.ts
@@ -0,0 +1,23 @@
+// Librería de conexión a la base de datos
+// Libraría externa: https://sidorares.github.io/node-mysql2/docs/documentation/typescript-examples
+import mysql from "mysql2/promise";
+
+// Configuración de la conexión a la base de datos
+import config from "config";
+
+/**
+ * Crear una conexión a la base de datos
+ * @returns Conexión a la base de datos
+ */
+
+const configOptions = {
+  host: config.get<string>("HOST"),
+  user: config.get<string>("USER"),
+  password: config.get<string>("PASSWORD"),
+  database: config.get<string>("DATABASE"),
+  port: config.get<number>("DB_PORT"),
+};
+export const getPoolConnection = () => {
+  const connection = mysql.createPool(configOptions);
+  return connection;
+};

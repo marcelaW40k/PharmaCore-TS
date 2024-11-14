78572029b09ab527f89b4dd0a20718337ada5b85
Camila Acosta <camiacofer@gmail.com>
2024-11-14 18:08:17 -0500

    Feat: creacion de sales.repository y modificacion inicial para tener en cuenta el array de items y almacenarlo en al tabla item_sales

--- src/infrastructure/repositories/sale.repository.ts
+++ src/infrastructure/repositories/sale.repository.ts
@@ -1,4 +1,5 @@
 import { Sale } from "../../domain/models/sale";
+import { SaleItem } from "../../domain/models/saleItem";
 import { FieldPacket, Pool, ResultSetHeader, RowDataPacket } from "mysql2/promise";
 import { Imanageable } from "../../domain/models/Imanager/interface";
 import { getPoolConnection } from "./config/data.source";
@@ -6,10 +7,21 @@ import { getPoolConnection } from "./config/data.source";
 export class SaleRepository implements Imanageable<Sale> {
     async create(body: Sale): Promise<ResultSetHeader> {
         const connection: Pool = getPoolConnection();
-        const querySql: string = `INSERT INTO sales (id_patient, date_time, sale_total_cost) VALUES (?,?,?)`;
+        const querySale = 'INSERT INTO sales (id_patient, date_time, sale_total_cost) VALUES (?,?,?)';
         const values: Array<string | number | Date> = [body.id_patient, body.date_time, body.sale_total_cost];
-        const result: [ResultSetHeader, FieldPacket[]] = await connection.query(querySql, values);
-        return result[0];
+        const saleResult: [ResultSetHeader, FieldPacket[]] = await connection.query(querySale, values);
+
+        const saleId = saleResult[0].insertId
+
+        const queryItems = 'INSERT INTO item_sales (id_sale, id_medicine, quantity, total_cost_item) VALUES (?,?,?,?)';
+        const valuesItems = body.items.map((item: SaleItem) => [
+            saleId,
+            item.id_medicine,
+            item.quantity,
+            item.total_cost_item
+        ]);
+        const saleItemResult: [ResultSetHeader, FieldPacket[]] = await connection.query<ResultSetHeader>(queryItems, [valuesItems]);
+        return saleResult[0];
     }
 
     async read(): Promise<RowDataPacket[]> {

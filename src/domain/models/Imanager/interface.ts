0e1a5002fdba0cf259aa0631632d022384790a82
Marcela <ardilamarcela42@gmail.com>
2024-11-14 15:40:06 -0500

    Modificando la interface

--- src/domain/models/Imanager/interface.ts
+++ src/domain/models/Imanager/interface.ts
@@ -1,14 +1,10 @@
-import { FieldPacket, Pool, ResultSetHeader, RowDataPacket } from "mysql2/promise";
-
+import {ResultSetHeader, RowDataPacket } from "mysql2/promise";
 export interface Imanageable <T>  {
     create(body:T):Promise<ResultSetHeader>;
- 
     read():Promise<RowDataPacket[]>;
-    searcheById(id:number):Array<any>;
-    delet(id:any):Promise<ResultSetHeader>;
+    searcheById(id:number):Promise<RowDataPacket[]>;
+    delete(id:number):Promise<ResultSetHeader>;
     update(body:T):Promise<ResultSetHeader>;
-    
-
 }
 
 

3a69e00244e8b1a757b53e7c69b6618f469c485d
Camila Acosta <camiacofer@gmail.com>
2024-11-14 22:31:27 -0500

    Feat: creación de la funcion calculateTotalCost en la clase Sale, para calcular el costo en lugar de pasarlo directamente.

--- src/domain/models/sale.ts
+++ src/domain/models/sale.ts
@@ -14,14 +14,20 @@ export class Sale {
             id_sale?: number
             id_patient: number,
             date_time: Date,
-            sale_total_cost: number,
             items: Array<SaleItem>
         }) {
         this.id_sale = infoSale.id_sale
         this.id_patient = infoSale.id_patient
         this.date_time = infoSale.date_time
-        this.sale_total_cost = infoSale.sale_total_cost
+        this.sale_total_cost = this.calculateTotalCost()
         this.items = infoSale.items
 
+    }
+    private calculateTotalCost() {
+        let totalCost = 0
+        this.items.forEach(item => {
+            totalCost += item.total_cost_item * item.quantity
+        })
+        return totalCost
     }
 }

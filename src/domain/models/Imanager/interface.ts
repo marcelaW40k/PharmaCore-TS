f8daee6cc137739004f075bd4db17d77ccddc086
SebasMejia1 <sebastianmejiasanchez1030@gmail.com>
2024-11-15 10:07:26 -0500

    fix: Correccion tipado interface

--- src/domain/models/Imanager/interface.ts
+++ src/domain/models/Imanager/interface.ts
@@ -1,10 +1,10 @@
 import {ResultSetHeader, RowDataPacket } from "mysql2/promise";
 export interface Imanageable <T>  {
-    create(body:T):Promise<T>;
-    read():Promise<T>;
-    searcheById(id:number):Promise<T>;
-    delete(id:number):Promise<T>;
-    update(body:T):Promise<T>;
+    create(body:T):Promise<T[]>;
+    read():Promise<T[]>;
+    searcheById(id:number):Promise<T[]>;
+    delete(id:number):Promise<T[]>;
+    update(body:T):Promise<T[]>;
 }
 
 

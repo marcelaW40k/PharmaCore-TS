9b4b15265c1f98474eb9c2ffe1be43d41570753b
Camila Acosta <camiacofer@gmail.com>
2024-11-18 15:20:10 -0500

    Feat: correción de SaleItem.repository con la Imanageable corregida

--- src/infrastructure/repositories/saleItem.repository.ts
+++ src/infrastructure/repositories/saleItem.repository.ts
@@ -4,51 +4,67 @@ import { getPoolConnection } from "./config/data.source";
 import { Imanageable } from "../../domain/models/Imanager/Imanageable";
 
 export class SaleItemRepository implements Imanageable<SaleItem> {
-    async create(body: SaleItem): Promise<any> {
-        const connection: Pool = getPoolConnection();
-        const querySale = 'INSERT INTO item_sales (id_sale, id_medicine, quantity, total_cost_item) VALUES (?,?,?,?)';
-        const values: Array<string | number | Date> = [body.id_sale, body.id_medicine, body.quantity, body.item_total_cost];
-        const Result: [ResultSetHeader, FieldPacket[]] = await connection.query(querySale, values);
-        return Result[0];
+
+    async create(body: SaleItem): Promise<SaleItem | null> {
+        const connection = getPoolConnection();
+        const querySql: string = 'INSERT INTO sale_items (id_sale, id_medicine, quantity, total_cost_item) VALUES (?,?,?,?)';
+        const values: Array<string | number> =
+            [body.id_sale,
+            body.id_medicine,
+            body.quantity,
+            body.item_total_cost];
+        ;
+        const result: [ResultSetHeader, FieldPacket[]] = await connection.query(
+            querySql,
+            values
+        );
+        body.id_item = result[0].insertId;
+        return result[0].affectedRows == 1 ? body : null;
     }
 
-    async read(): Promise<any> {
+    async read(): Promise<SaleItem[]> {
         const connection = getPoolConnection();
-        const querySql = `SELECT * FROM item_sales`;
-        const result = await connection.query<RowDataPacket[]>(querySql);
-        return result[0];
-    }
-
-    async searcheById(id: number): Promise<any> {
-        const connection = getPoolConnection();
-        const querySql = `SELECT * FROM item_sales WHERE id_item = ?`;
-        const values = [id]
-        const result = await connection.query<RowDataPacket[]>(querySql, values);
-        return result[0];
-    }
-
-
-    async remove(id: number): Promise<any> {
-        const connection = getPoolConnection();
-        const querySql = `DELETE FROM item_sales WHERE id_item =?`;
-        const values = [id]
-        const result: [ResultSetHeader, FieldPacket[]] = await connection.query(querySql, values);
-        return result[0];
-
+        const querySql = `SELECT * FROM sale_items`;
+        const result = await connection.query(querySql);
+        return result[0] as SaleItem[]
     }
 
 
-    async update(body: SaleItem): Promise<any> {
+    async searchById(id: number): Promise<SaleItem | null> {
         const connection = getPoolConnection();
-        const querySql = `UPDATE item_sales SET id_sale= ?, id_medicine = ?, quantity = ?, item_total_cost =?, WHERE id_item = ?`;
+        const querySql: string = `SELECT   * FROM sale_items WHERE id = ?`;
+        const values = [id];
+        const result: [RowDataPacket[], FieldPacket[]] = await connection.query(querySql, values);
+        return result[0].length > 0 ? result[0][0] as SaleItem : null;
+    }
+
+
+
+    async remove(id: number): Promise<boolean> {
+        const connection = getPoolConnection();
+        const querySql = ` DELETE FROM sale_items WHERE id = ?`;
+        const values = [id];
+        const result: [ResultSetHeader, FieldPacket[]] = await connection.query(
+            querySql,
+            values
+        );
+
+        return result[0].affectedRows == 1 ? true : false;
+    }
+
+    async update(body: SaleItem): Promise<SaleItem | null> {
+        const connection = getPoolConnection();
+        const querySql = `UPDATE sale_items SET id_sale= ?, id_medicine = ?, quantity = ?, item_total_cost =?, WHERE id_item = ?`;
         const values = [
             body.id_sale,
             body.id_medicine,
             body.quantity,
-            body.item_total_cost,
-        ];
-        const result: [ResultSetHeader, FieldPacket[]] = await connection.query(querySql, values);
-        return result[0];
-    }
+            body.item_total_cost,];
+        const result = await connection.query<ResultSetHeader>(querySql, values);
 
+        body.id_item = result[0].insertId;
+        return result[0].affectedRows == 1 ? body : null;
+
+
+    }
 }

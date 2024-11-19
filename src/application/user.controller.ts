8c21395d056dd3e5d04b0b5acf80788222261027
wen A <florezaraujo@hotmail.com>
2024-11-18 20:47:45 -0500

    se crean los controllers, queda pendienye update por dto

--- src/application/user.controller.ts
+++ src/application/user.controller.ts
@@ -1,50 +1,96 @@
-import { ResultSetHeader } from "mysql2";
-import { User } from "../domain/models/doctor";
-import { UserRepository } from "../infrastructure/repositories/config/doctor.repository";
-import { error } from "console";
 
-export class UserController  { // agregar la interface
-  private repository: UserRepository;
+import { Doctor } from "../domain/models/doctor";
+import { doctorRepository } from "../infrastructure/repositories/config/doctor.repository";
+import { Imanageable } from "../domain/models/Imanager/Imanageable";
+
+
+export class UserController implements Imanageable<Doctor> {
+  // agregar la interface
+  private repository: doctorRepository;
 
   constructor() {
-    this.repository = new UserRepository();
+    this.repository = new doctorRepository();
   }
 
-  // async create(body: {
-  //   idUser: number;
+  async create(body: Doctor): Promise<Doctor | null> {
+    try {
+      const doctor = new Doctor(body);
+      const result = await this.repository.create(doctor);
+      if (result && result.id_doctor == 1) {
+        console.log(`se agregó con exito`);
+        return result;
+      } else {
+        console.log(` no se agregó con exito`);
+        return null;
+      }
+    } catch (error: any) {
+      console.log("ha ocurrido un erro al agregar.", error.message);
+    }
+    return body;
+  }
+
+  async read(): Promise<Doctor[]> {
+    const result: Doctor[] = await this.repository.read();
+    if (result.length == 1) {
+      console.log(" obtenidos");
+    } else {
+      console.log("no se encontró");
+    }
+
+    return result;
+  }
+
+  async searchById(id: number): Promise<Doctor | null> {
+    try {
+      const resultado = await this.repository.searchById(id);
+      if (resultado) {
+        return resultado;
+      } else {
+        return null;
+      }
+    } catch (error: any) {
+      console.log("Ha ocurrido un error inesperado", error.message);
+      return null;
+    }
+  }
+
+  async remove(id: number): Promise<boolean> {
+    try {
+      const resultado: any = await this.repository.remove(id);
+
+      if (resultado === true) {
+        console.log(` eliminado`);
+        return true;
+      } else {
+        console.log(`no se pudo eliminar el ${id}`);
+        return false;
+      }
+    } catch (error) {
+      console.log(`Ocurrio un error inesperado con ${id}`);
+      return false;
+    }
+  }
+    //pendiente dto
+  // async update(body: {
+  //   id_doctor: number;
   //   email: string;
   //   password: string;
-  //   idRole: number;
-  // }){
-  //   // este body debe ser completamente string, por que son los valores que vienen de postman
+  //   id_role: number;
+  // }): Promise<Doctor | null> {
   //   try {
-  //     const dto = new userDto(body); // pendiente por hacer el dto
-  //     const errores = await dto.validateDto();
-  //     if (errores.Length > 0) {
-  //           return {
-  //           ok: false,
-  //           message: "el request tiene errores",
-  //           error: errores,
-  //           };
-  //       }
+  //     const update = new updateDto(body);
+  //     const errores = await update.validateDto();
 
-  //     const user = new User(body);
-  //     const result = await this.repository.create(user);
-  //     if (result.affectedRows == 1) {
-  //       return { ok: false, message: "ha ocurrido un error inesperado", error };
-  //     } else {
-  //       return { ok: false, message: "el usuario no se agregó" };
+  //     if (errores.length > 0) {
+  //       console.log("usuario actualizado");
+  //       throw new Error("Hubo un eror al validar los datos ");
   //     }
-  //   } catch (error: any) {
-  //     throw { ok: false, message: "ha ocurrido un error inesperado", error };
+
+  //     const user = new Doctor(body);
+  //     const result = await this.repository.update(user);
+  //     return result;
+  //   } catch (error) {
+  //     throw error;
   //   }
   // }
-
-  async read() {
-       const result = await this.repository.read()
-        // if(result.length == 0)
-        console.log("usuarios obtenidos")
-        console.log(result[0])
-        return result ;
-  }
 }

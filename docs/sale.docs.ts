423e2c8b9b32c6d5d10a2fcf12b4da58e4399508
Camila Acosta <camiacofer@gmail.com>
2024-11-25 15:19:57 -0500

    Creación de sale.docs

---
+++ docs/sale.docs.ts
@@ -0,0 +1,82 @@
+export const saleDoc = {
+    get: {
+        tags: ["Ventas"],
+        summary: "Obtener lista de ventas",
+        description: "Obtiene una lista de todos las ventas generadas",
+        responses: {
+            "200": {
+                description: "Medicamentos obtenidos correctamente",
+                content: {
+                    "application/json": {
+                        schema: {
+                            type: "array",
+                            items: {
+                                $ref: "#/components/schemas/Medicine"
+                            }
+                        }
+                    }
+                }
+            },
+            "500": {
+                description: "Error interno del servidor"
+            }
+        }
+    },
+    post: {
+        tags: ["Medicamentos"],
+        summary: "Crear un nuevo medicamento",
+        description: "Crea un nuevo medicamento en la base de datos",
+        requestBody: {
+            content: {
+                "application/json": {
+                    schema: {
+                        $ref: "#/components/schemas/Medicine"
+                    }
+                }
+            }
+        },
+        responses: {
+            "201": {
+                description: "Medicamento creado correctamente",
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schemas/Medicine"
+                        }
+                    }
+                }
+            },
+            "400": {
+                description: "Error al validar los datos"
+            },
+            "500": {
+                description: "Error interno del servidor"
+            }
+        }
+    },
+    put: {
+        tags: ["Medicamentos"],
+        summary: "Actualizar un medicamento",
+        description: "Actualiza un medicamento en la base de datos",
+        parameters: [
+            {
+                name: "id",
+                in: "path",
+                required: true,
+                schema: {
+                    type: "string"
+                }
+            }
+        ],
+        requestBody: {
+            content: {
+                "application/json": {
+                    schema: {
+                        $ref: "#/components/schemas/Medicine"
+                    }
+                }
+            }
+        },
+    },
+
+}

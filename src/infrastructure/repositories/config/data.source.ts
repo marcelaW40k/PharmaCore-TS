de215055ce31237fa1c703d8ac1c61abac2c48f8
Marcela <ardilamarcela42@gmail.com>
2024-11-27 18:33:37 -0500

    puerto

--- src/infrastructure/repositories/config/data.source.ts
+++ src/infrastructure/repositories/config/data.source.ts
@@ -7,7 +7,7 @@ const configOptions = {
   user: config.get<string>("DB_USER"),
   password: config.get<string>("DB_PASSWORD"),
   database: config.get<string>("DATABASE"),
-  port: Number(config.get<string>("DB_PORT")) || 3306,
+  port: Number(config.get<string>("DB_PORT")) || 43649,
 };
 export const getPoolConnection = () => {
   const connection = mysql.createPool(configOptions);
